99cbbfc4e1511aa4a9ab61e4f2f50cddae28481a
Shivangi-Gohel <shivangigohel18@gmail.com>
2025-05-06 18:19:50 +0530

    implement HeroSection component in App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Login from './pages/Login'
+import HeroSection from './pages/student/HeroSection'
 
 function App() {
 
   return (
     <main>
       <Navbar/>
+      <HeroSection/>
       <Login />
     </main>
   )
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -24,7 +24,7 @@ import {
 import { Separator } from "@radix-ui/react-dropdown-menu";
 
 const Navbar = () => {
-  const user = false; // Replace with actual user authentication logic
+  const user = true; // Replace with actual user authentication logic
   return (
     <div className="h-16 dark:bg-[#0A0A0A] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
       {/* Desktop */}
